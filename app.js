be0d362eb9f87a9d6ecf6522b3cf7c5fb98fb495
biccon <admin@idx.kr>
2018-11-09 23:28:38 +0900

    server ready!

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-import a from './test';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { importSchema } from "graphql-import";
+import resolvers from "./graphql/resolvers";
 
-console.log('print a', a);
+const typeDefs = importSchema("./graphql/schema.graphql");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+);
